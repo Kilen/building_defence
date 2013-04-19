9dcb29a6065adfa36048814ac869540c79fa12fb
kilen <kilen@gitcafe.com>
2013-04-19 15:23:17 +0800

    make dictionary load words from /dictionary/brit-a-z

--- dictionary.rb
+++ dictionary.rb
@@ -3,12 +3,8 @@ module BuildingDefence
     def initialize
       @words = []
       @max_len = 0
-      @number = 0
-      load_words.each do |word|
-        @words << word 
-        @max_len = word.length if word.length > @max_len
-        @number += 1
-      end
+      @number = 20
+      load_words
     end
 
     def max_len
@@ -27,14 +23,13 @@ module BuildingDefence
     private
 
     def load_words
-      %w(
-        apple like girl man jack tom light weight building
-        Converts any arguments to arrays then merges elements 
-        self with corresponding elements from each argument
-        new array can be created by using the literal constructor 
-        Arrays can contain different types of objects For example 
-        the array below contains an Integer String and Float
-      )
+      dic = IO.readlines "dictionary/brit-a-z"
+      @number.times do
+        word  = dic[rand(dic.length)].chop
+        @max_len = word.length if word.length > @max_len
+        @words << word
+      end
     end
   end
 end
+
